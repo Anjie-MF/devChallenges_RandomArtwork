7b81aecf937933a738332efa9057662d9f36527f
Anjelica <133686543+Anjie-MF@users.noreply.github.com>
2026-06-28 15:48:13 -0500

    Test: random placeholder image

--- js/script.js
+++ js/script.js
@@ -53,8 +53,11 @@ const getArtwork = async function (query = "painting") {
         // document.getElementById("pictureOfArt").innerHTML = `<img src="https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg" alt="${title}" style="max-width: 100%;">`;
         const iiifUrl = data.config.iiif_url;
 
+        // document.getElementById("pictureOfArt").innerHTML =
+        // `<img src="${iiifUrl}/${imageId}/full/full/0/default.jpg" alt="${title}" style="max-width: 100%;">`   TESTING KEEP THIS IN IF OTHER TEST DOENT WORK;
+
         document.getElementById("pictureOfArt").innerHTML =
-        `<img src="${iiifUrl}/${imageId}/full/full/0/default.jpg" alt="${title}" style="max-width: 100%;">`;
+`<img src="https://picsum.photos/600/400" alt="test" style="max-width:100%;">`;
         
     } catch (error) {
         console.error("Failed to fetch artwork:", error);
